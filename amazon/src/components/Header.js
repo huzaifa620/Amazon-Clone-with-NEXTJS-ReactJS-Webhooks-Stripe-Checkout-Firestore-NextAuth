4c3ef33967995130206b696d07088f0c4c43ac88
Ghost620 <79418234+Ghost620@users.noreply.github.com>
2022-11-04 17:52:17 +0500

    logo added

--- amazon/src/components/Header.js
+++ amazon/src/components/Header.js
@@ -1,8 +1,25 @@
+import Image from "next/image"
+
 const Header = () => {
   return (
-    <div>
-      
-    </div>
+    <header>
+
+      <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
+
+        <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
+          <Image src='https://pngimg.com/uploads/amazon/amazon_PNG11.png' alt='' width={150} height={40} objectFit='contain' className="cursor-pointer"/>  
+        </div> 
+
+        <div>
+          <input type="text" />
+        </div>
+
+      </div>
+
+      <div>
+
+      </div>
+    </header>
   )
 }
 
