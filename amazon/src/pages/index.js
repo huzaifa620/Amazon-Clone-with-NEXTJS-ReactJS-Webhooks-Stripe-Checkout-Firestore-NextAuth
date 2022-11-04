dce209a0bb790c970ad5a21397d7ae41c39bd375
Ghost620 <79418234+Ghost620@users.noreply.github.com>
2022-11-04 17:08:21 +0500

    header

--- amazon/src/pages/index.js
+++ amazon/src/pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
@@ -6,7 +7,10 @@ export default function Home() {
       <Head>
         <title>Amazon 2.0</title>
       </Head>
-      HH
+      
+      <Header>
+        
+      </Header>
     </div>
   );
 }
