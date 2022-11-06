b90a13f6be1f3692d4c95dc7e24ba95af5f41b2a
Ghost620 <79418234+Ghost620@users.noreply.github.com>
2022-11-06 19:13:03 +0500

    header done

--- amazon/src/components/Header.js
+++ amazon/src/components/Header.js
@@ -1,5 +1,5 @@
 import Image from "next/image"
-import { MenuIcon, MagnifyingGlassIcon, ShoppingCartIcon } from "@heroicons/react/24/outline"
+import { Bars3Icon, MagnifyingGlassIcon, ShoppingCartIcon } from "@heroicons/react/24/outline"
 
 const Header = () => {
   return (
@@ -8,7 +8,7 @@ const Header = () => {
       <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
 
         <div className="mt-2 flex items-center flex-grow sm:flex-grow-0 px-6">
-          <Image src='https://pngimg.com/uploads/amazon/amazon_PNG11.png' alt='' width={150} height={40} objectFit='contain' className="cursor-pointer"/>  
+          <Image src='https://pngimg.com/uploads/amazon/amazon_PNG11.png' alt='' width={150} height={40} objectFit='contain' className="link"/>  
         </div> 
 
         <div className="hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer bg-yellow-400 hover:bg-yellow-500">
@@ -40,7 +40,22 @@ const Header = () => {
 
       </div>
 
-      <div>
+      <div className="flex items-center space-x-3 p-2 pl-6 bg-amazon_blue-light text-white text-sm">
+
+        <p className="link flex items-center">
+          <Bars3Icon className="h-6 mr-1" />
+          All
+        </p>
+
+        <p className="link"> Prime Video </p>
+        <p className="link"> Amazon Business </p>
+        <p className="link"> Today's Deals </p>
+        <p className="link hidden lg:inline-flex"> Electronics </p>
+        <p className="link hidden lg:inline-flex"> Food & Grocery </p>
+        <p className="link hidden lg:inline-flex"> Prime </p>
+        <p className="link hidden lg:inline-flex"> Buy Again </p>
+        <p className="link hidden lg:inline-flex"> Shopper Toolkit </p>
+        <p className="link hidden lg:inline-flex"> Health & Personal Care </p>
 
       </div>
     </header>
