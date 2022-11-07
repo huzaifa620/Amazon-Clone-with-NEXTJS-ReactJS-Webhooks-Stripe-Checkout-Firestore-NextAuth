8c0075fb33f242932c4d6d3ef2d0a894a61364cc
Ghost620 <79418234+Ghost620@users.noreply.github.com>
2022-11-08 00:52:18 +0500

    products added

--- amazon/src/components/ProductFeed.js
+++ amazon/src/components/ProductFeed.js
@@ -1,9 +1,15 @@
+import Product from "./Product"
 
-
-const ProductFeed = () => {
+const ProductFeed = ( {products} ) => {
   return (
     <div>
-        ProductFeed
+        {
+        products.map( ({id, title, price, description, category, image}) => (
+
+          <Product key={id} id={id} title={title} price={price} description={description} category={category} image={image} />
+
+        ))
+        }
     </div>
   )
 }
