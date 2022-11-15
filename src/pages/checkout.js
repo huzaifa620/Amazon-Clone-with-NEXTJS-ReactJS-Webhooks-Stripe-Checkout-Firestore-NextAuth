2967f915d8048973d5869eeee70b9a421680c2e0
Ghost620 <79418234+Ghost620@users.noreply.github.com>
2022-11-15 20:24:41 +0500

    total added in checkout

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,12 +1,13 @@
 import Header from "../components/Header"
 import { useSelector } from "react-redux"
-import { selectItems } from "../slices/basketSlice"
+import { selectItems, selectTotal } from "../slices/basketSlice"
 import CheckoutProduct from "../components/CheckoutProduct"
 import Currency from "react-currency-formatter"
 import { useSession } from "next-auth/react"
 
 const checkout = () => {
   const items = useSelector(selectItems)
+  const total = useSelector(selectTotal)
   const { data: session } = useSession();
 
   return (
@@ -31,16 +32,18 @@ const checkout = () => {
 
         </div>
 
-        <div>
+        <div className="flex flex-col bg-white p-10 shadow-md">
           {items.length > 0 && (
             <>
               <h2 className="whitespace-nowrap">
                 Subtotal ({items.length} items):
-                {/* <span className="font-bold"> <Currency quantity={total} /> </span> */}
+                <span className="font-bold"> <Currency quantity={total} /> </span>
               </h2>
 
-              <button className={`button mt-2 ${!session && 'from-gray-300 to-gray-500 border-gray-200 text-gray-300 cursor-not-allowed'}`}>
+              <button disabled={!session} className={`button mt-2 ${!session && 'from-gray-300 to-gray-500 border-gray-200 text-gray-300 cursor-not-allowed'}`}>
+
                 {!session ? "Sign in to checkout" : "Proceed to checkout"}
+
               </button>
             </>
           )}
