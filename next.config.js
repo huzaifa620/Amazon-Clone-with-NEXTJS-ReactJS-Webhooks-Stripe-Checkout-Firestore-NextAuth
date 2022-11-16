829a0dfc42ee6971e19c9b507ae274c4e866a438
Ghost620 <79418234+Ghost620@users.noreply.github.com>
2022-11-16 23:08:17 +0500

    stripe setup

--- next.config.js
+++ next.config.js
@@ -3,5 +3,8 @@ module.exports = {
   reactStrictMode: true,
   images: {
     domains: ["pngimg.com", "fakestoreapi.com"]
+  },
+  env: {
+    stripe_public_key: process.env.STRIPE_PUBLIC_KEY
   }
 }
--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -4,11 +4,21 @@ import { selectItems, selectTotal } from "../slices/basketSlice"
 import CheckoutProduct from "../components/CheckoutProduct"
 import Currency from "react-currency-formatter"
 import { useSession } from "next-auth/react"
+import { loadStripe } from "@stripe/stripe-js"
+
+const stripePromise = loadStripe(process.env.stripe_public_key)
 
 const checkout = () => {
   const items = useSelector(selectItems)
   const total = useSelector(selectTotal)
   const { data: session } = useSession();
+
+  const createCheckoutSession = async () => {
+    const stripe = await stripePromise
+
+    // Call the backend to create a checkout session...
+    const checkoutSession = await axios.post("/api/")
+  }
 
   return (
     <div className="bg-gray-100">
@@ -40,7 +50,7 @@ const checkout = () => {
                 <span className="font-bold"> <Currency quantity={total} /> </span>
               </h2>
 
-              <button disabled={!session} className={`button mt-2 ${!session && 'from-gray-300 to-gray-500 border-gray-200 text-gray-300 cursor-not-allowed'}`}>
+              <button role="link" onClick={createCheckoutSession} disabled={!session} className={`button mt-2 ${!session && 'from-gray-300 to-gray-500 border-gray-200 text-gray-300 cursor-not-allowed'}`}>
 
                 {!session ? "Sign in to checkout" : "Proceed to checkout"}
 
