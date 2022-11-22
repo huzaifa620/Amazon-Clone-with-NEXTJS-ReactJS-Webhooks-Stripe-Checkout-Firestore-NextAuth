3635ed09d77747e7d03c7fe14de16ffd77672aad
Ghost620 <79418234+Ghost620@users.noreply.github.com>
2022-11-22 20:50:04 +0500

    stripe updated

--- src/pages/api/create-checkout-session.js
+++ src/pages/api/create-checkout-session.js
@@ -5,23 +5,45 @@ export default async (req, res) => {
 
     const transformedItems = items.map((item) => (
         {
-            description: item.description,
             quantity: 1,
             price_data: {
                 currency: "usd",
                 unit_amount: item.price * 100,
                 product_data: {
                     name: item.title,
-                    images: [item.image]
-                }
-            }
+                    description: item.description,
+                    images: [item.image],
+                },
+            },
         }
     ))
 
     const session = await stripe.checkout.sessions.create(
         {
             payment_method_types: ["card"],
-            shipping_rates: ['shr_1M6AvBCzu6a96DIo5oMkF9gU'],
+            mode: 'payment',
+            shipping_options: [
+                {
+                shipping_rate_data: {
+                    type: 'fixed_amount',
+                    fixed_amount: {
+                        amount: 699, // in cents
+                        currency: 'usd',
+                    },
+                    display_name: 'Next Day Shipping',
+                    delivery_estimate: {
+                    minimum: {
+                        unit: 'business_day',
+                        value: 1,
+                    },
+                    maximum: {
+                        unit: 'business_day',
+                        value: 3,
+                    },
+                    },
+                },
+                },
+            ],
             shipping_address_collection: {
                 allowed_countries: ["GB", "US", "CA"]
             },
@@ -32,7 +54,8 @@ export default async (req, res) => {
             metadata: {
                 email,
                 images: JSON.stringify(items.map((item) => item.image))
-            }
+            },
+
         }
     )
 
