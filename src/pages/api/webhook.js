28018683b8d3dcf9a1fb00c3d312dafbb5d4e661
Ghost620 <79418234+Ghost620@users.noreply.github.com>
2022-11-23 23:01:39 +0500

    Update webhook.js

--- src/pages/api/webhook.js
+++ src/pages/api/webhook.js
@@ -12,13 +12,36 @@ const app = !admin.apps.length ? admin.initializeApp({
 // Establish connction to Stripe
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
 
-const edpointSecret = process.env.STRIPE_SIGNING_SECRET 
+const endpointSecret = process.env.STRIPE_SIGNING_SECRET
+
+const fulfillOrder = async (session) => {
+    return app
+    .firestore()
+    .collection('users').doc(session.metadata.email)
+    .collection("orders").doc(session.id)
+}
 
 
 export default async (req, res) => {
 
     if (req.method === "POST"){
-        
+        const requestBuffer = await buffer(req)
+        const payload = requestBuffer.toString()
+        const sig = req.headers["stripe-signature"]
+
+        let event;
+        // Verify that the event posted came from stripe
+        try {
+            event = stripe.webhooks.constructEvent(payload, sig, edpointSecret)
+        } catch (error) {
+            console.log('ERROR', error.message)
+            return res.status(400).send(`Webhook error: ${error.message}`)
+        }
+
+        if (event.type === "checkout.session.completed") {
+            const session = event.data.object
+        }
+
     }
 
 }
