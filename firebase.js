c18798a5ad67350f2ff481870646e5d4897389f0
Ghost620 <79418234+Ghost620@users.noreply.github.com>
2022-12-22 01:08:09 +0500

    orders page continued

--- firebase.js
+++ firebase.js
@@ -1,4 +1,5 @@
-import firebase from "firebase"
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/firestore'
 
 const firebaseConfig = {
     apiKey: "AIzaSyBkiqoahufkgbyEXGuGtrgwSmdSB19nBcM",
@@ -8,3 +9,9 @@ const firebaseConfig = {
     messagingSenderId: "458808454139",
     appId: "1:458808454139:web:c8a7fdf7ba70cae362795e"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+
+export default db
---
+++ src/components/Order.js
@@ -0,0 +1,33 @@
+import moment from "moment/moment"
+import Currency from "react-currency-formatter"
+
+const Order = ( {id, amount, amountShipping, items, timestamp, images } ) => {
+  return (
+    <div className="relative border rounded-md">
+        <div className="flex items-center space-x-10 p-5 bg-gray-100 text-sm text-gray-600">
+
+            <div>
+                <p className="font-bold text-xs"> ORDER PLACED </p>
+                <p>{moment.unix(timestamp).format("DD MMM YYY")}</p>
+            </div>
+
+            <div>
+                <p className="text-xs font-bold">TOTAL</p>
+                <p>
+                    <Currency quantity={amount} /> - Next Day Delivery{" "}
+                    <Currency quantity={amountShipping} />
+                </p>
+            </div>
+
+            <p className="text-sm whitespace-nowrap sm:text-xl self-end flex-1 text-right text-blue-500">{items.length} items</p>
+
+            <p className="absolute top-2 right-2 w-40 lg:w-72 truncate text-xs whitespace-nowrap">
+                ORDER # {id}
+            </p>
+
+        </div>
+    </div>
+  )
+}
+
+export default Order
